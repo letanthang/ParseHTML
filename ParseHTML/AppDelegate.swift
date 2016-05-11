2abfbd9f08afc6f3a1ba1d8cf5b6fe5d22b19850
Tan Thang Le <letanthang@gmail.com>
2016-05-11 08:20:46 +0700

    Initial Commit

---
+++ ParseHTML/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ParseHTML
+//
+//  Created by Le Tan Thang on 5/11/16.
+//  Copyright © 2016 Le Tan Thang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ParseHTML/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ParseHTML
+//
+//  Created by Le Tan Thang on 5/11/16.
+//  Copyright © 2016 Le Tan Thang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
